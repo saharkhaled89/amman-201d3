54c72cebbdbbf097e92ea8d317e085cb177de098
sahar khaled <engineer.sahar.8989@hotmail.com>
2020-02-20 18:45:35 +0200

    add

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -83,7 +83,9 @@ testSumAndMultiply(4,7,5);
 
 /////////////////////////////////////
 /* Problem 4
-Write a function called sumArray() that takes in an array of numbers as its single argument and then returns an array where the first element is the sum of the numbers in the array, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
+Write a function called sumArray() that takes in an array of numbers as its single argument and then 
+returns an array where the first element is the sum of the numbers in the array, and the second element is 
+a string that EXACTLY follows this example and uses the values that were input into the function:
 
 "2,3,4 was passed in as an array of numbers, and 9 is their sum."
 
@@ -96,11 +98,22 @@ var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
 
+  var sum_total = 0;
+
+  for(var i = 0; i < sumArr.length ; i++){
+    sum_total = sum(sum_total, sumArr[i])[0];
+  }
+
+  return [sum_total ,sumArr +' was passed in as an array of numbers, and ' + sum_total + ' is their sum.'] ;
+
+
 }
+
+
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
